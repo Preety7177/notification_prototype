301268006fc26a78bbbba3ca9c9c3655a4ddc89e
gocallum <callumbir@gmail.com>
2024-08-01 03:25:29 +1000

    changed the statushook.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -7,7 +7,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { User } from '@prisma/client';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Status } from './submit-button';
+import { Status } from './statushook';
 
 export default async function AdminPage() {
   const users: User[] = await fetchUsers();
